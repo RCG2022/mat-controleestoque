e2d17f6f41418a237b3a48d3ad7fb109e3a4647c
Cesar <cesarc9h@hotmail.com>
2022-03-29 00:41:15 -0300

    adicionado botão no formulario

--- src/MAT.ControleEstoque.App/Form1.cs
+++ src/MAT.ControleEstoque.App/Form1.cs
@@ -6,5 +6,10 @@ namespace MAT.ControleEstoque.App
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello world"); 
+        }
     }
 }
