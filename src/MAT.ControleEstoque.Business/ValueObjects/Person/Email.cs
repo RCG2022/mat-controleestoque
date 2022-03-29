cdfc417b0d2b7182ceaa3b1783beb80d67aa2b78
rauny <rauny.marques2211@outlook.com>
2022-03-28 22:35:11 -0300

    Validaçoes Address e e-mail

--- src/MAT.ControleEstoque.Business/ValueObjects/Person/Email.cs
+++ src/MAT.ControleEstoque.Business/ValueObjects/Person/Email.cs
@@ -32,7 +32,7 @@ namespace MAT.ControleEstoque.Business.ValueObjects.Person
             if (string.IsNullOrEmpty(email))
                 return false;
 
-            return Regex.IsMatch(email, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+            return Regex.IsMatch(email, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");  
         }
     }
 }
