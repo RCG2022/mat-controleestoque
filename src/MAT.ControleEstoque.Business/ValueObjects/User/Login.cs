c28e2369409d08509d5ffff9b27d99b697d40d98
Rafael <rafaflp2013@gmail.com>
2022-03-29 23:30:00 -0300

    Implementação dos Objetos de Valor e Teste (Login e Passwor)

---
+++ src/MAT.ControleEstoque.Business/ValueObjects/User/Login.cs
@@ -0,0 +1,23 @@
+﻿namespace MAT.ControleEstoque.Business.ValueObjects.User
+{
+    public class Login
+    {
+
+        private const string INVALID_LENGTH_MIN = "O login deve ter no minimo 3 caracter";
+        private const string INVALID_LENGTH_MAX = "O login deve ter no máximo 20 caracter";
+
+        public String Value { get; private set; }
+
+        public Login(string value)
+        {
+            if(value.Length < 3 )
+                throw new ArgumentException(INVALID_LENGTH_MIN);
+
+            if(value.Length > 20)
+                throw new ArgumentException(INVALID_LENGTH_MAX);
+
+            Value = value;
+        }
+        
+    }
+}
