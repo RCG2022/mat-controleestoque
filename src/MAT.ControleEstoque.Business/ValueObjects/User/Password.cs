9e939c81c91104074260df159822979916757360
Paulo Cesar de Souza <paulocesaaars@gmail.com>
2022-04-04 20:57:29 -0300

    Teste Password

--- src/MAT.ControleEstoque.Business/ValueObjects/User/Password.cs
+++ src/MAT.ControleEstoque.Business/ValueObjects/User/Password.cs
@@ -1,39 +1,60 @@
-﻿using System.Text.RegularExpressions;
+﻿using Deviot.Common;
+using System.Text.RegularExpressions;
 
 namespace MAT.ControleEstoque.Business.ValueObjects.User
 {
     public class Password
     {
-        private const string INVALID_LENGTH_MIN = "O Password deve ter no mínimo 6 caracteres";
-        private const string INVALID_LENGTH_MAX = "O Password deve ter no máximo 20 caracteres";
-        private const string INVALID_PASSWORD = "O Password deve ter no mínimo 1 caracter especial," +
-            " uma letra Maiúscula, uma letra minúscula e 1 número";
-        
+        public String Value { get; private set; }
 
-        public string Value { get; private set; }
+        private const string INVALID_LENGTH_MIN = "A senha deve ter no mínimo 6 caracteres";
+        private const string INVALID_LENGTH_MAX = "A senha deve ter no máximo 20 caracteres";
+        private const string MUST_CONTAIN_ONE_LOWERCASE_LETTER = "A senha deve conter ao menos uma letra minúscula";
+        private const string MUST_CONTAIN_ONE_UPPERCASE_LETTER = "A senha deve conter ao menos uma letra maiúscula";
+        private const string MUST_CONTAIN_ONE_SPECIAL_CHARACTERS = "A senha deve conter ao menos um caractere especial";
+        private const string INVALID_CHARACTERS = "A senha deve conter somente letras, números e os caracteres $*&@#";
 
-        public Password(string valeu) {
-
-            if (valeu.Length < 6)
+        public Password(string value)
+        {
+            if (value.Length < 6)
                 throw new ArgumentException(INVALID_LENGTH_MIN);
 
-            if (valeu.Length > 200)
+            if (value.Length > 20)
                 throw new ArgumentException(INVALID_LENGTH_MAX);
 
-            if (ValidatePassword(valeu) == false)
-                throw new ArgumentException(INVALID_PASSWORD);
-            
-            Value = valeu;
+            if (!validateLowercase(value))
+                throw new ArgumentException(MUST_CONTAIN_ONE_LOWERCASE_LETTER);
 
+            if (!validateUppercase(value))
+                throw new ArgumentException(MUST_CONTAIN_ONE_UPPERCASE_LETTER);
+
+            if (!validateSpecialCharacters(value))
+                throw new ArgumentException(MUST_CONTAIN_ONE_SPECIAL_CHARACTERS);
+
+            if (!validateCharacters(value))
+                throw new ArgumentException(INVALID_CHARACTERS);
+
+            Value = Utils.Encript(value);
         }
 
-        private static bool ValidatePassword(string password)
+        private static bool validateLowercase(string value)
         {
-            if (string.IsNullOrEmpty(password))
-                return false;
-
-            return Regex.IsMatch(password, @"^(?=.*[!@#$%¨&*()_+])(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
+            return value.Length > Regex.Replace(value, "[a-z]", string.Empty).Length;
         }
 
+        private static bool validateUppercase(string value)
+        {
+            return value.Length > Regex.Replace(value, "[A-Z]", string.Empty).Length;
+        }
+
+        private static bool validateSpecialCharacters(string value)
+        {
+            return value.Length > Regex.Replace(value, "[$*&@#]", string.Empty).Length;
+        }
+
+        private static bool validateCharacters(string value)
+        {
+            return Regex.IsMatch(value, @"^[a-zA-Z0-9$*&@#]*$");
+        }
     }
 }
