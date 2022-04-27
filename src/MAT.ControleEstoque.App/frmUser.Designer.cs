46f5589f7c8895c94f7679bf33cc3407ada4e53b
Cesar <cesarc9h@hotmail.com>
2022-04-26 22:06:15 -0300

    Arrumando Merge

--- src/MAT.ControleEstoque.App/frmUser.Designer.cs
+++ src/MAT.ControleEstoque.App/frmUser.Designer.cs
@@ -181,16 +181,14 @@
             // 
             // txtLogin
             // 
-            this.txtLogin.BackColor = System.Drawing.Color.White;
-            this.txtLogin.Location = new System.Drawing.Point(77, 62);
+            this.txtLogin.Location = new System.Drawing.Point(77, 57);
             this.txtLogin.Name = "txtLogin";
             this.txtLogin.Size = new System.Drawing.Size(300, 23);
             this.txtLogin.TabIndex = 11;
             // 
             // txtPassword
             // 
-            this.txtPassword.BackColor = System.Drawing.Color.White;
-            this.txtPassword.Location = new System.Drawing.Point(77, 91);
+            this.txtPassword.Location = new System.Drawing.Point(77, 86);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(300, 23);
             this.txtPassword.TabIndex = 12;
